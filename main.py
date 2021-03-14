60a7b4f1d66012ea2a1db1ac3a60d292e3f97994
Jochem Bruijninckx <jochembruijninckx@gmail.com>
2021-03-14 22:04:40 +0100

    Final commit

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from Problem import Problem, gen_instance
 from Solver import *
-
+import winsound
 
 # Task to run
 # --------------------------------------------------------------------------------------
-instance_name = 'large_data_set'   # Enter a number to generate a random instance
+instance_name = 'small_data_set'  # Enter a number to generate a random instance
 method = 'heuristic'                     # Options are 'read', 'solve', 'heuristic'
 
 # Task settings (only used if method is 'heuristic')
 # --------------------------------------------------------------------------------------
-create_initial_solution = True
+create_initial_solution = True         # If False, the initial solution is loaded from an existing file
 heuristic_settings = {
     'model_parameters': {
-        'boundary': 10,
-        'delta': 0.25,
+        'boundary': 2.5,                # B
+        'delta': 0.25,                     # Delta_B
     },
     'step_1': {
-        'epsilon': 0.001,
+        'epsilon': 0.001,               # Optimality gap stopping criterion for Step 1
+        'time': 120,
         'surpress_gurobi': False
     },
     'step_2': {
-        'start_capacity': 2.5,
-        'capacity_step': 0.25
+        'start_capacity': 2.5,          # m
+        'capacity_step': 0.25           # Delta_m
     },
     'step_3': {
-        'check_full_list': False
+        'check_full_list': False        # If True, the best improvement from the entire list is chosen on each iteration
     },
     'step_4': {
-        'epsilon': 0.01,
+        'epsilon': 0.001,               # Optimality gap stopping criterion for Step 4
+        'time': 7200,
         'surpress_gurobi': False
     }
 }
@@ -38,8 +40,8 @@ if instance_name not in ['small_data_set', 'large_data_set']:
     # This function can be called to generate an .xlsx instance file
     gen_instance(seed=int(instance_name),
                  num_s=6,
-                 num_d=8,
-                 num_c=16,
+                 num_d=6,
+                 num_c=12,
                  num_p=1,
                  T=20)
     instance_name = str(instance_name)
@@ -57,7 +59,7 @@ elif method == 'heuristic':
 
 # Log functions for solution
 # --------------------------------------------------------------------------------------
-problem.verify_constraints()
 problem.log_objective(summary_only=True)
-problem.display()
+problem.display(integer=True)
+
 input('Press enter to exit..')
